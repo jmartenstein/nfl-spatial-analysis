aa7069d4cfa6a92de9d5812d1e73e839ac8f1fff
Justin Martenstein <jmartenstein@knockcrm.com>
2025-08-17 16:22:23 -0700

    first attempt at putting the Pitch Control Model into code

---
+++ src/wide_open.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+# wide_open.py - implementation of Bornn and Fernandez's Pitch Control Model
+
+import numpy as np
+import pandas as pd
+
+import scipy.stats as st
+
+def get_influence_radius(distance):
+
+    # compute the influence radius, based on the distance from the player
+    # to the ball
+
+    radius = np.minimum( (3 / (200 * (distance**2))) + 4, 10)
+
+def get_covariance_matrix(direction, speed, distance_from_ball):
+
+    # first compute the rotation matrix for direction (see Appendix 1, Equation 16 
+    # in Bornn and Fernandez)
+
+    # R = [ [ cos(theta), -sin(theta) ], [ sin(theta), cos(theta) ] ]
+    th = np.radians(direction)
+    rotation = np.array( [[np.cos(th), -np.sin(th)]
+                          [np.sin(th), np.cos(th)]]
+                       )
+
+    # next compute the scaling matrix for speed (Appendix 1, Eq. 17)
+
+    max_speed = 18
+    inf_radius = get_influence_radius(distance_from_ball)
+
+    # Appendix 1, Eq. 18)
+    speed_ratio = (speed**2) / (max_speed**2)
+
+    # Appendix 1, Equation 17 and Equation 19
+    # S = [ [scale_x, 0], [0, scale_y]]
+    scale_x = (inf_radius + (inf_radius*speed_ratio) / 2)
+    scale_y = (inf_radius - (inf_radius*speed_ratio)) / 2)
+    scaling = np.array( [scale_x, 0], [0, scale_y]] )
+
+    # multiply the rotation and scaling matrices, per equation 15 
+
+    # COV = RSSR^-1
+    covariance = rotation @ scaling @ scaling @ rotation.T
+
+def get_player_influence_func(position, direction, speed):
+
+    cov = get_covariance_matrix(direction, speed)
+
+    speed_x = speed * np.cos(np.pi * (direction / 180))
+    speed_y = speed * np.sin(np.pi * (direction / 180))
+
+    # per Appendix 1, Equation 21 calculate the gaussian
+    # distribution mean as the X and Y position plus half
+    # of the current speed
+
+    # mu = position + speed * 0.5
+
+    dist_mean = np.array([
+        position[0] + (speed_x * 0.5),
+        position[1] + (speed_y * 0.5)
+    ])
+
+    distribution = st.multivariate_normal(dist_mean, cov)
+
+    return distribution.pdf(position)
